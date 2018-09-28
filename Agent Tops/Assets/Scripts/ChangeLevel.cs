39be201bce22793bf519bc8c990556e23506dd72
DunkeyDunk <Malthe23@live.dk>
2018-09-28 13:06:10 +0200

    Added Door and ChangeLevel Script

---
+++ Agent Tops/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour {
+
+    bool inTrigger = false;
+    public string scene;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.X) && inTrigger == true)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Door")
+        {
+            inTrigger = true;
+        }
+	}
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Door")
+        {
+            inTrigger = false;
+        }
+    }
+}
