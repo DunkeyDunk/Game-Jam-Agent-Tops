667c56cfc96ba09c99bf921deb4b76cd6cb1b8ee
DunkeyDunk <Malthe23@live.dk>
2018-09-28 15:08:23 +0200

    Started on Caught Script
    I started on the caught script and added a light cone. if the player stands in it you lose

---
+++ Agent Tops/Assets/Scripts/Caught.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caught : MonoBehaviour {
+
+    GameObject[] pausedObj;
+
+    private void Start()
+    {
+        pausedObj = GameObject.FindGameObjectsWithTag("ShowWhenPaused");
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StartCoroutine(waitAndPause());
+            
+        }
+    }
+
+    IEnumerator waitAndPause()
+    {
+        foreach (GameObject g in pausedObj)
+        {
+            print(g);
+            g.SetActive(true);
+        }
+        print(Time.time);
+        yield return new WaitForSecondsRealtime(1);
+        print(Time.time);
+        Time.timeScale = 0;
+    }
+
+}
