ca92070c33bb810222916ef63c39778e28b246ad
DunkeyDunk <Malthe23@live.dk>
2018-09-28 14:38:03 +0200

    Grounded check
    Makes it so you have to be grounded to launch yourself

--- Agent Tops/Assets/Scripts/PlayerLaunch.cs
+++ Agent Tops/Assets/Scripts/PlayerLaunch.cs
@@ -6,22 +6,25 @@ public class PlayerLaunch : MonoBehaviour {
 
     public GameObject player;
     public float power;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private bool grounded;
+    public float minLaunchHight;
+        
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Z))
+		if (Input.GetKeyDown(KeyCode.Z) && grounded)
         {
+
             //find spillerens position som vector
             Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
             //find musens position vector og minus spillere vectoren
             Vector2 launch = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)) - playerPos;
             launch.Normalize();
             //give spilleren fart
-            GetComponent<Rigidbody2D>().velocity += launch * power;
+            if (launch.y >= minLaunchHight)
+            {
+                GetComponent<Rigidbody2D>().velocity += launch * power;
+                grounded = false;
+            }
         }
 
         
@@ -33,6 +36,7 @@ public class PlayerLaunch : MonoBehaviour {
         {
             //hvis man rammer mister man sin fart
             GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            grounded = true;
         }
     }
 }
