ef09b6c8459ad6c253ca158f20ccf7effe1eff7d
DunkeyDunk <Malthe23@live.dk>
2018-10-07 18:32:55 +0200

    Lauch code Change
    Nu har det en betydning for hvor langt væk musen er fra spilleren.
    Der er også blevet sat et maks på så man ikke bare flyver sindsygt langt hvis musen er i kanten af skærmen

--- Agent Tops/Assets/Scripts/PlayerLaunch.cs
+++ Agent Tops/Assets/Scripts/PlayerLaunch.cs
@@ -6,28 +6,45 @@ public class PlayerLaunch : MonoBehaviour {
 
     public GameObject player;
     public float power;
-    private bool grounded;
+    private bool grounded = true;
     public float minLaunchHight;
+    public float maxX;
+    public float maxY;
         
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Z) && grounded)
         {
-            float i = 0f;
+            //float i = 0f;
             //find spillerens position som vector
             Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
             //find musens position vector og minus spillere vectoren
             Vector2 launch = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)) - playerPos;
-            launch.Normalize();
             //give spilleren fart
-            while (Input.GetKey(KeyCode.Z))
+            //while (Input.GetKey(KeyCode.Z))
+            //{
+            //    i = i + Time.deltaTime;
+            //    print(i + " time has passed");
+            //}
+            print(launch.x.ToString() + "x " + launch.y.ToString() + "y :Before");
+            if (launch.x >= maxX)
             {
-                i = i + Time.deltaTime;
-                print(i + " time has passed");
+                launch.x = maxX;
             }
+            if (launch.y >= maxY)
+            {
+                launch.y = maxY;
+            }
+            if (launch.x <= -maxX)
+            {
+                launch.x = -maxX;
+            }
+            print(launch.x.ToString() + "x " + launch.y.ToString() + "y :After");
+
             if (launch.y >= minLaunchHight)
             {
-                GetComponent<Rigidbody2D>().velocity += launch * i;
+                GetComponent<Rigidbody2D>().velocity += launch * power;
+
                 grounded = false;
             }
         }
