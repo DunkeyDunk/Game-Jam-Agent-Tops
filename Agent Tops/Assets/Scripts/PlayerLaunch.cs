e6bd0de84e676740daee27173d3ddf01d47f2599
DunkeyDunk <Malthe23@live.dk>
2018-10-09 09:46:13 +0200

    StartScreen creation

--- Agent Tops/Assets/Scripts/PlayerLaunch.cs
+++ Agent Tops/Assets/Scripts/PlayerLaunch.cs
@@ -10,22 +10,17 @@ public class PlayerLaunch : MonoBehaviour {
     public float minLaunchHight;
     public float maxX;
     public float maxY;
-        
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Z) && grounded)
+    Vector2 launch;
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKey(KeyCode.Z) && grounded)
         {
-            //float i = 0f;
             //find spillerens position som vector
             Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
             //find musens position vector og minus spillere vectoren
-            Vector2 launch = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)) - playerPos;
+            launch = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)) - playerPos;
             //give spilleren fart
-            //while (Input.GetKey(KeyCode.Z))
-            //{
-            //    i = i + Time.deltaTime;
-            //    print(i + " time has passed");
-            //}
             print(launch.x.ToString() + "x " + launch.y.ToString() + "y :Before");
             if (launch.x >= maxX)
             {
@@ -41,7 +36,17 @@ public class PlayerLaunch : MonoBehaviour {
             }
             print(launch.x.ToString() + "x " + launch.y.ToString() + "y :After");
 
-            if (launch.y >= minLaunchHight)
+
+            //if (launch.y >= minLaunchHight)
+            //{
+            //    GetComponent<Rigidbody2D>().velocity += launch * power;
+
+            //    grounded = false;
+            //}
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            if (launch.y >= minLaunchHight && grounded)
             {
                 GetComponent<Rigidbody2D>().velocity += launch * power;
 
@@ -49,8 +54,8 @@ public class PlayerLaunch : MonoBehaviour {
             }
         }
 
-        
-	}
+
+    }
     //checker om man rammer jorden
     void OnCollisionEnter2D(Collision2D col)
     {
