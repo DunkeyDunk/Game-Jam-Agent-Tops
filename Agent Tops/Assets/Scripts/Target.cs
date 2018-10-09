8a728ff45d04fda6c1f9c9b82a74eea7978bed3c
DunkeyDunk <Malthe23@live.dk>
2018-10-09 14:46:19 +0200

    Change level change to teleport player
    nu teleporter den spilleren i stedet for at skifte scene.

---
+++ Agent Tops/Assets/Scripts/Target.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public GameObject target;
+	
+    public Vector2 GetTarget()
+    {
+        Vector2 pos;
+        pos = target.GetComponent<Transform>().position;
+        return (pos);
+    }
+}
